633b21d3a5757725c8f50e7daec7041c6abdb87f
DaniyarMussatayev <mussatayevdt@gmail.com>
2020-07-03 14:21:25 +0600

    Task 95

--- src/com/syntax/repl89_107/Repl95.java
+++ src/com/syntax/repl89_107/Repl95.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class Repl95 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String word = scan.next();
-		System.out.printf("The first 3 letters of %s is %s", word, word.substring(0, 3));
-		scan.close();
+		String name = scan.nextLine();
+		
+		System.out.println("The first 3 letters of " + name + " is " + name.substring(0,3));
 	}
 
 }
@@ -18,3 +18,5 @@ public class Repl95 {
 //Print out the following: "The first 3 letters of ___ is ___"
 //
 //For example, if the input is "banana", your output should be: "The first 3 letters of banana is ban".
+
+
