85038f1cbfad38a8eab82f01617ec1feeb4dd5ee
DaniyarMussatayev <mussatayevdt@gmail.com>
2020-07-04 18:40:03 +0600

    Task 105

--- src/com/syntax/repl89_107/Repl105.java
+++ src/com/syntax/repl89_107/Repl105.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Repl105 {
 	public static void main(String[] args) {
-		Scanner inp = new Scanner(System.in);
-		System.out.print("In:");
-		String word = inp.nextLine();
-		// write your code below
-		System.out.println(word.replaceAll("[^aouei]", ""));
+		 Scanner inp = new Scanner(System.in);
+		    System.out.print("In:");
+		    String word = inp.nextLine();
+		 
+		    System.out.println(word.replaceAll("[^aouei]", ""));
 	}
 }
 
