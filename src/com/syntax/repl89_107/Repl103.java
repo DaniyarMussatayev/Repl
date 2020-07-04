b61b34611f52fe50f62ca150a34a62824c54020e
DaniyarMussatayev <mussatayevdt@gmail.com>
2020-07-04 18:12:34 +0600

    Task 103

--- src/com/syntax/repl89_107/Repl103.java
+++ src/com/syntax/repl89_107/Repl103.java
@@ -3,16 +3,13 @@ package com.syntax.repl89_107;
 import java.util.Scanner;
 
 public class Repl103 {
-	public static void main(String[] args) {
-		Scanner inp = new Scanner(System.in);
-		System.out.print("In:");
-		String s = inp.nextLine();
-		// write your code below
-		for (int i = 0; i < s.length(); i++) {
-			System.out.print(s.charAt(i) + " ");
-			
-		}
-
+	 public static void main(String[] args) {
+		 Scanner inp = new Scanner(System.in);
+		    System.out.print("In:");
+		    String word = inp.nextLine();
+		    for (int i = 0; i<word.length(); i++) {
+		    	System.out.print(word.charAt(i) + "");
+		    }
 	}
 }
 
