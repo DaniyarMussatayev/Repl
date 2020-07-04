7734b3896986a08544952e20c4ea1177a6a98b07
DaniyarMussatayev <mussatayevdt@gmail.com>
2020-07-04 18:07:24 +0600

    Task 101

--- src/com/syntax/repl89_107/Repl101.java
+++ src/com/syntax/repl89_107/Repl101.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Repl101 {
 	public static void main(String[] args) {
 		Scanner inp = new Scanner(System.in);
-		System.out.print("In:");
-		String word = inp.nextLine();
-		// write your code below
-
-		for (int i = 0; i < word.length(); i += 2) {
-			System.out.print(word.charAt(i));
-		}
-
+	    System.out.print("In:");
+	    String word = inp.nextLine();
+	    
+	    for(int i=0;i<word.length(); i++) {
+	    	if (i%2==0) {
+	    		System.out.print(word.charAt(i));
+	    	}
+	    }
 	}
 }
 
