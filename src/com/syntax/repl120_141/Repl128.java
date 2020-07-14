b65d70ccfb329ff82504ff7e1cd3e40fecdf52a7
DaniyarMussatayev <mussatayevdt@gmail.com>
2020-07-14 14:16:51 +0600

    Repl120 141

--- src/com/syntax/repl120_141/Repl128.java
+++ src/com/syntax/repl120_141/Repl128.java
@@ -2,13 +2,7 @@ package com.syntax.repl120_141;
 
 public class Repl128 {
 	public static void main(String[] args) {
-		String firstValue = mixString("12345", "abcde");
-		System.out.println(firstValue);
-		String secondValue = mixString("howdy", "hello");
-		System.out.println(secondValue);
-	}
-
-	static String mixString(String s1, String s2) {
+		
 		
 
 	}
